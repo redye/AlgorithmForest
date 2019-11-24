7a221324859cf7f27d6f46c3ca54996524c8edea
redye <mac@redyedeMacBook-Pro.local>
2019-11-24 20:54:38 +0800

    merge linked list

--- Algorithm.playground/Pages/MergeLinkedList.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/MergeLinkedList.xcplaygroundpage/Contents.swift
@@ -2,26 +2,6 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
-
-let a1 = [1, 2, 4]
-var l1 = ListNode(0)
-for x in a1 {
-    l1.next = ListNode(x)
-    l1 = l1.next!
-}
-l1 = l1.next!
-print(l1)
-
-let a2 = [1, 3, 4]
-var l2 = ListNode(0)
-for x in a2 {
-    l2.next = ListNode(x)
-    l2 = l2.next!
-}
-l2 = l2.next!
-print(l2)
-
+let l1 = createLinkedList([1, 2, 4])
+let l2 = createLinkedList([2, 5, 6])
 mergeTwoLists(l1, l2)
