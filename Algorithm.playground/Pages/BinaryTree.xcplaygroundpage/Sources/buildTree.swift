4e4b60ea1ec629adef7c334b1f2aba07c08b2e46
redye <sixwalnut@qq.com>
2019-12-05 23:16:07 +0800

    从前序与中序遍历序列构造二叉树

--- Algorithm.playground/Pages/BinaryTree.xcplaygroundpage/Sources/buildTree.swift
+++ Algorithm.playground/Pages/BinaryTree.xcplaygroundpage/Sources/buildTree.swift
@@ -2,6 +2,12 @@ import Foundation
 
 /// 题目链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
+/// 递归
+/// 前序：根 -  左 - 右
+/// 中序：左 - 根 - 右
+/// 前序第一个元素是根结点
+/// 中序根结点之前是左子树的中序序列，之后是右子树中序序列
+/// 前序根结点之后左子树个数的前序序列，左子树之后是右子树的前序序列
 public func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
     if preorder.count <= 0 || inorder.count <= 0 {
         return nil
