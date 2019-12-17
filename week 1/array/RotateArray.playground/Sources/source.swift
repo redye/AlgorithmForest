8b66932679520d81d04fccc762b4e2f7b8c91359
redye <sixwalnut@qq.com>
2019-12-17 17:28:27 +0800

    rotate array

--- week 1/array/RotateArray.playground/Sources/source.swift
+++ week 1/array/RotateArray.playground/Sources/source.swift
@@ -16,12 +16,13 @@ import Foundation
 /**
  * 三次翻转
  * [1,2,3,4,5,6,7]
- * [7,6,5,4,3,2,1]
- * [5,6,7,4,3,2,1]
- * [5,6,7,1,2,3,4]
+ * => [7,6,5,4,3,2,1]
+ * => [5,6,7,4,3,2,1]
+ * => [5,6,7,1,2,3,4]
  * 时间复杂度 O(n)
  */
 public func rotate(_ nums: inout [Int], _ k: Int) {
+    let j = k % nums.count
     func reverse(_ nums: inout [Int],_ start: Int, _ end: Int) {
         var low = start
         var high = end
@@ -34,6 +35,58 @@ public func rotate(_ nums: inout [Int], _ k: Int) {
         }
     }
     reverse(&nums, 0, nums.count - 1)
-    reverse(&nums, 0, k - 1)
-    reverse(&nums, k, nums.count - 1)
+    reverse(&nums, 0, j - 1)
+    reverse(&nums, j, nums.count - 1)
+}
+
+/**
+ * 暴力解法
+ * 循环移动
+ * 时间复杂度 O(k*n)
+ */
+public func rotate2(_ nums: inout [Int], _ k: Int) {
+    let count = k % nums.count
+    for _ in 0..<count {
+        let tmp = nums[nums.count - 1]
+        var j = nums.count - 1
+        while j > 0 {
+            nums[j] = nums[j - 1]
+            j -= 1
+        }
+        nums[0] = tmp
+    }
+}
+
+
+/**
+ * 环状替换
+ * [1,2,3,4,5,6,7] 和 k = 3
+ * => [5, 6, 7, 1, 2, 3, 4]
+ * 第0号位换到第3号位
+ * 第1号位换到第4号位
+ * 第2号位换到第5号位
+ * ....
+ * 第 i 号位换到底 len - k - 1 号位
+ *
+ * 第 len - k 号位换到第 0 位
+ * 第 len - k + 1 号位换到第 1 位
+ */
+public func rotate3(_ nums: inout [Int], _ k: Int) {
+    
+}
+
+/**
+ * 使用额外的数组，将元素放在正确的位置上
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(n)
+ */
+public func rotate4(_ nums: inout [Int], _ k: Int) {
+    var tmp = [Int](repeating: 0, count: nums.count)
+    for i in 0..<nums.count {
+        let index = (i + k) % nums.count
+        tmp[index] = nums[i]
+    }
+    for i in 0..<nums.count {
+        nums[i] = tmp[i]
+    }
 }
